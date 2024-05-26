d4cbe8e6fc81ec14974e9a0a30e354828ec87a24
Leon Liang <ycliang.cn@outlook.com>
2024-05-26 19:47:26 +0100

    1. update TaskDetail.vue component 2. add Todo.vue drawers

--- src/components/TaskDetail.vue
+++ src/components/TaskDetail.vue
@@ -1,58 +1,51 @@
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue';
-import { Button } from '@/components/ui/button';
+import { ref } from 'vue';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { Calendar } from '@/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-import { ChevronRight, List, Ban, Cross, Check, X, CalendarDays, Tag, ListTodo } from 'lucide-vue-next';
-import { parseDate, DateFormatter, type DateValue, getLocalTimeZone, isEqualDay, today } from '@internationalized/date';
-import { cn, getCurrentTime } from '@/lib/utils';
-
-const props = defineProps({
-  task: {
-    type: Object as () => Task,
-    required: true,
-  },
-  lists: {
-    type: Array as () => TaskList[],
-    required: true,
-  },
-});
-
-watch(() => props.task, () => {
-  taskIndex.value = props.task.id;
-  taskDescription.value = props.task.description;
-  taskNote.value = props.task.note;
-  taskBelongList.value = props.task.list_id + '';
-});
-
-watch(() => props.lists, () => {
-  taskLists.value = props.lists;
-  taskBelongList.value = props.task.list_id + '';
-});
-
-const emit = defineEmits(['updateTask']);
-
-const showDueDatePopover = ref(false);
-const editTemp = ref<any>();
-
-const taskLists = ref(props.lists);
-const taskIndex = ref(props.task.id);
-const taskDescription = ref(props.task.description);
-const taskNote = ref(props.task.note);
-const taskDueDate = ref<DateValue>();
-const taskBelongList = ref(props.task.list_id + '');
+import { Tag, CalendarDays, List, Check, X } from 'lucide-vue-next';
+import {
+  DateFormatter,
+  type DateValue,
+  getLocalTimeZone,
+  isEqualDay,
+  today,
+} from '@internationalized/date';
+import { cn } from '@/lib/utils';
 
 const df = new DateFormatter('zh-cn', { dateStyle: 'long' });
 
-const updateDueDate = (date: DateValue | undefined) => {
+const taskIndex = defineModel<number>('taskIndex');
+const taskDescription = defineModel<string>('taskDescription');
+const taskNote = defineModel<string>('taskNote');
+const taskDueDate = defineModel<DateValue>('taskDueDate');
+const taskCreateTime = defineModel<string>('taskCreateTime');
+const taskUpdateTime = defineModel<string>('taskUpdateTime');
+const taskFinishTime = defineModel<string>('taskFinishTime');
+const taskBelongList = defineModel<string>('taskBelongList');
+const lists = defineModel<TaskList[]>('lists');
+
+const showDueDatePopover = ref(false);
+const editTemp = ref();
+const calendarDate = ref<DateValue>();
+
+const emits = defineEmits(['update']);
+
+
+const saveDueDate = (date: DateValue | undefined) => {
   showDueDatePopover.value = false;
   if (!date) return;
   taskDueDate.value = date;
-  emit('updateTask', { due_date: date.toString() });
+  emits('update', taskIndex.value, { due_date: date.toString() });
+};
+
+const updateTaskList = () => {
+  if (!taskBelongList.value) return;
+  emits('update', taskIndex.value, { list_id: parseInt(taskBelongList.value) });
 };
 
 const editGate = (v: any, e: Function) => {
@@ -66,100 +59,104 @@ const editGate = (v: any, e: Function) => {
 </script>
 
 <template>
-  <div>
-    <div class="grid gap-2 border-t-2 mt-4 py-4">
-      <div class="flex items-center">
-        <Tag class="size-4 mr-3" />
-        <Input
-            class="w-full pl-4"
-            v-model="taskDescription"
-            placeholder="任务名称"
-            @keyup.enter="$event.target.blur()"
-            @focus="editTemp = taskDescription"
-            @blur="editGate(taskDescription, () => {
-            emit('updateTask', taskIndex, { description: taskDescription })
-          })"
-        />
-      </div>
-      <div class="flex items-center">
-        <CalendarDays class="size-4 mr-3" />
-        <Popover v-model:open="showDueDatePopover">
-          <PopoverTrigger as-child>
-            <Button :class="cn('grow justify-start', !taskDueDate && 'text-muted-foreground')" variant="outline">
-              {{ taskDueDate ? df.format(taskDueDate.toDate(getLocalTimeZone())) : "设置截止日期" }}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent class="h-[300px] flex w-auto flex-col gap-y-2 p-2 overflow-auto">
-            <Button
-                v-if="!taskDueDate || !isEqualDay(taskDueDate, today(getLocalTimeZone()))"
-                variant="outline"
-                class="w-full justify-start"
-                @click="updateDueDate(today(getLocalTimeZone()))"
-            >
-              <CalendarDays class="mr-2 h-4 w-4" />今天截止
-            </Button>
-            <Button
-                v-if="!taskDueDate || !isEqualDay(taskDueDate, today(getLocalTimeZone()).add({ days: 1 }))"
-                variant="outline"
-                class="w-full justify-start"
-                @click="updateDueDate(today(getLocalTimeZone()).add({ days: 1 }))"
-            >
-              <CalendarDays class="mr-2 h-4 w-4" />明天截止
-            </Button>
-            <Calendar class="-mt-2" v-model="taskDueDate" @update:modelValue="updateDueDate(taskDueDate)" />
-          </PopoverContent>
-        </Popover>
-        <Button class="ml-2 text-red-500" v-if="taskDueDate" variant="outline" size="icon" @click="showDueDatePopover = false; taskDueDate = undefined;">
-          <X class="size-5" />
-        </Button>
-      </div>
-      <div class="flex items-center">
-        <List class="size-4 mr-3" />
-        <div class="grow">
-          <Select v-model="taskBelongList" @update:modelValue="(val) => emit('updateTask', { list_id: parseInt(val) })">
-            <SelectTrigger>
-              <SelectValue class="ml-1" placeholder="选择列表" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem v-for="list in taskLists" :key="list.id" :value="list.id+''">
-                {{ list.name }}
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
-      <Textarea
-          class="min-h-24"
-          v-model="taskNote"
-          placeholder="添加备注"
-          @focus="editTemp = taskNote"
-          @blur="editGate(taskNote, () => {
-          emit('updateTask', { note: taskNote })
+  <div class="grid gap-2 py-4">
+    <div class="flex items-center">
+      <Tag class="size-4 mr-3" />
+      <Input
+          class="w-full pl-4"
+          v-model="taskDescription"
+          placeholder="任务名称"
+          @keyup.enter="$event.target.blur()"
+          @focus="editTemp = taskDescription"
+          @blur="editGate(taskDescription, () => {
+          emits('update', taskIndex, { description: taskDescription });
         })"
       />
     </div>
+    <div class="flex items-center">
+      <CalendarDays class="size-4 mr-3" />
+      <Popover v-model:open="showDueDatePopover">
+        <PopoverTrigger as-child>
+          <Button :class="cn('grow justify-start', !taskDueDate && 'text-muted-foreground')" variant="outline">
+            {{ taskDueDate ? df.format(taskDueDate.toDate(getLocalTimeZone())) : "设置截止日期" }}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent class="h-[300px] flex w-auto flex-col gap-y-2 p-2 overflow-auto">
+          <Button
+              v-if="!taskDueDate || !isEqualDay(taskDueDate, today(getLocalTimeZone()))"
+              variant="outline"
+              class="w-full justify-start"
+              @click="saveDueDate(today(getLocalTimeZone()))"
+          >
+            <CalendarDays class="mr-2 h-4 w-4" />今天截止
+          </Button>
+          <Button
+              v-if="!taskDueDate || !isEqualDay(taskDueDate, today(getLocalTimeZone()).add({ days: 1 }))"
+              variant="outline"
+              class="w-full justify-start"
+              @click="saveDueDate(today(getLocalTimeZone()).add({ days: 1 }))"
+          >
+            <CalendarDays class="mr-2 h-4 w-4" />明天截止
+          </Button>
+          <Calendar class="-mt-2" v-model="calendarDate" @update:modelValue="saveDueDate(calendarDate)" />
+        </PopoverContent>
+      </Popover>
+      <Button
+          class="ml-2 text-red-500"
+          v-if="taskDueDate"
+          variant="outline"
+          size="icon"
+          @click="showDueDatePopover = false; taskDueDate = undefined;"
+      >
+        <X class="size-5" />
+      </Button>
+    </div>
+    <div class="flex items-center">
+      <List class="size-4 mr-3" />
+      <div class="grow">
+        <Select
+            v-model="taskBelongList"
+            @update:modelValue="updateTaskList"
+        >
+          <SelectTrigger>
+            <SelectValue class="ml-1" placeholder="选择列表" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem v-for="list in lists" :value="list.id+''">{{ list.name }}</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </div>
+    <Textarea
+        class="min-h-24"
+        v-model="taskNote"
+        placeholder="添加备注"
+        @focus="editTemp = taskNote"
+        @blur="editGate(taskNote, () => {
+        emits('update', taskIndex, { note: taskNote });
+      })"
+    />
     <div class="text-xs text-gray-500 italic">
       <div>
-        <span v-if="!props.task.update_time">创建于 {{ props.task.create_time }}</span>
+        <span v-if="!taskUpdateTime">创建于 {{ taskCreateTime }}</span>
         <TooltipProvider v-else>
           <Tooltip>
             <TooltipTrigger as-child>
-              <span>最后更新于 {{ props.task.update_time }}</span>
+              <span>最后更新于 {{ taskUpdateTime }}</span>
             </TooltipTrigger>
             <TooltipContent side="top">
-              创建于 {{ props.task.create_time }}
+              创建于 {{ taskCreateTime }}
             </TooltipContent>
           </Tooltip>
         </TooltipProvider>
       </div>
-      <div class="mt-2 flex items-center" v-if="props.task.finish_time">
+      <div class="mt-2 flex items-center" v-if="taskFinishTime">
         <Check class="size-4 text-green-500 mr-2" />
-        完成于 {{ props.task.finish_time }}
+        完成于 {{ taskFinishTime }}
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-/* Add any required styles here */
 </style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,31 +2,11 @@
 import { ref, onMounted, computed } from 'vue';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Textarea } from '@/components/ui/textarea';
-import { Calendar } from '@/components/ui/calendar';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer'
-import {
-  parseDate,
-  DateFormatter,
-  type DateValue,
-  getLocalTimeZone,
-  isEqualDay,
-  today
-} from '@internationalized/date';
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger }
+  from '@/components/ui/drawer'
+import { ChevronRight, Ban, Cross, ListTodo } from 'lucide-vue-next';
+import { parseDate, type DateValue, } from '@internationalized/date';
 import { cn, getCurrentTime } from '@/lib/utils';
-import { ChevronRight, List, Ban, Cross, Check, X, CalendarDays, Tag, ListTodo } from 'lucide-vue-next';
 import TaskRecord from "@/components/TaskRecord.vue";
 import TaskLists from "@/components/TaskLists.vue";
 import TaskDetail from "@/components/TaskDetail.vue";
@@ -39,11 +19,10 @@ const lists = ref<TaskList[]>([]);
 const selectedLists = ref(new Set<number>());
 const completeExpanded = ref(false);
 const showAddDialog = ref(false);
+const showTaskDetailDrawer = ref(false);
 const showDeleteDialog = ref(false);
-const showDueDatePopover = ref(false);
 
 const newTaskDescription = ref('');
-const editTemp = ref();
 
 const taskIndex = ref(0);
 const taskDescription = ref('');
@@ -53,8 +32,6 @@ const taskCreateTime = ref();
 const taskUpdateTime = ref();
 const taskFinishTime = ref();
 const taskBelongList = ref('1');
-
-const df = new DateFormatter('zh-cn', { dateStyle: 'long' });
 
 // 获取所有任务和列表
 const fetchAllData = () => {
@@ -132,6 +109,9 @@ const handleTaskSelect = (task: Task) => {
   taskUpdateTime.value = task.update_time;
   taskFinishTime.value = task.finish_time;
   taskBelongList.value = task.list_id + '';
+  if (window.innerWidth <= 640) {
+    showTaskDetailDrawer.value = true;
+  }
 };
 
 const deleteTask = (id: number) => {
@@ -160,11 +140,6 @@ const updateComplete = (id: number, completed: boolean) => {
   fetchAllData();
 };
 
-// 更新任务列表
-const updateTaskList = () => {
-  updateTask(taskIndex.value, { list_id: parseInt(taskBelongList.value) });
-};
-
 // 切换选中列表
 const toggleListSelection = (listId: number) => {
   if (selectedLists.value.has(listId)) {
@@ -180,21 +155,13 @@ const handleExpandCompleted = () => {
   ipc.send('config-store-set', { 'todoCompleteExpanded': completeExpanded.value });
 };
 
-const saveDueDate = (date: DateValue | undefined) => {
-  showDueDatePopover.value = false;
-  if (!date) return;
-  taskDueDate.value = date;
-  updateTask(taskIndex.value, { due_date: date.toString() });
-};
-
-const editGate = (v: any, e: Function) => {
-  if (v === editTemp.value) {
-    return;
-  } else {
-    e();
+const onTaskDetailDrawerStateChange = () => {
+  if (!showTaskDetailDrawer.value) {
+    setTimeout(() => {
+      cleanTaskInfo();
+    }, 300);
   }
-  editTemp.value = null;
-}
+};
 
 onMounted(() => {
   fetchAllData();
@@ -208,7 +175,7 @@ onMounted(() => {
     <div class="flex-1 relative">
       <div class="w-full h-full pb-20 overflow-auto">
         <div class="flex items-center gap-2 sm:hidden px-8 sticky top-0 h-14 w-full bg-white/40 backdrop-blur-sm">
-          <span class="text-xl font-bold">任务清单</span>
+          <span class="text-xl font-bold">任务清单{{showTaskDetailDrawer}}</span>
           <Drawer>
             <DrawerTrigger as-child>
               <Button variant="ghost" size="icon">
@@ -217,10 +184,10 @@ onMounted(() => {
             </DrawerTrigger>
             <DrawerContent>
               <DrawerHeader class="hidden">
-                <DrawerTitle>Move Goal</DrawerTitle>
-                <DrawerDescription>aaa</DrawerDescription>
+                <DrawerTitle>任务列表</DrawerTitle>
+                <DrawerDescription>所有创建的列表</DrawerDescription>
               </DrawerHeader>
-              <div class="pl-4 pr-2 max-h-[200px] overflow-auto">
+              <div class="pl-4 pr-2 max-h-[60vh] overflow-auto">
                 <TaskLists
                     :lists="lists"
                     :selectedLists="selectedLists"
@@ -311,110 +278,45 @@ onMounted(() => {
         />
       </div>
       <transition name="fade">
-        <div v-if="taskIndex">
-          <div class="grid gap-2 border-t-2 mt-4 py-4" >
-            <div class="flex items-center">
-              <Tag class="size-4 mr-3" />
-              <Input
-                  class="w-full pl-4"
-                  v-model="taskDescription"
-                  placeholder="任务名称"
-                  @keyup.enter="$event.target.blur()"
-                  @focus="editTemp = taskDescription"
-                  @blur="editGate(taskDescription, () => {
-                  updateTask(taskIndex, { description: taskDescription })
-                })"
-              />
-            </div>
-            <div class="flex items-center">
-              <CalendarDays class="size-4 mr-3" />
-              <Popover v-model:open="showDueDatePopover">
-                <PopoverTrigger as-child>
-                  <Button
-                      :class="cn('grow justify-start', !taskDueDate && 'text-muted-foreground')"
-                      variant="outline"
-                  >
-                    {{ taskDueDate ? df.format(taskDueDate.toDate(getLocalTimeZone())) : "设置截止日期" }}
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent class="h-[300px] flex w-auto flex-col gap-y-2 p-2 overflow-auto">
-                  <Button
-                      v-if="!taskDueDate || !isEqualDay(taskDueDate, today(getLocalTimeZone()))"
-                      variant="outline"
-                      class="w-full justify-start"
-                      @click="saveDueDate(today(getLocalTimeZone()))"
-                  >
-                    <CalendarDays class="mr-2 h-4 w-4" />今天截止
-                  </Button>
-                  <Button
-                      v-if="!taskDueDate || !isEqualDay(taskDueDate, today(getLocalTimeZone()).add({ days: 1 }))"
-                      variant="outline"
-                      class="w-full justify-start"
-                      @click="saveDueDate(today(getLocalTimeZone()).add({ days: 1 }))"
-                  >
-                    <CalendarDays class="mr-2 h-4 w-4" />明天截止
-                  </Button>
-                  <Calendar class="-mt-2" v-model="taskDueDate" @update:modelValue="saveDueDate(taskDueDate)" />
-                </PopoverContent>
-              </Popover>
-              <Button
-                  class="ml-2 text-red-500"
-                  v-if="taskDueDate"
-                  variant="outline"
-                  size="icon"
-                  @click="showDueDatePopover = false; taskDueDate = undefined;"
-              >
-                <X class="size-5" />
-              </Button>
-            </div>
-            <div class="flex items-center">
-              <List class="size-4 mr-3" />
-              <div class="grow">
-                <Select v-model="taskBelongList" @update:modelValue="updateTaskList">
-                  <SelectTrigger>
-                    <SelectValue class="ml-1" placeholder="选择列表" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem v-for="list in lists" :value="list.id+''">
-                      {{ list.name }}
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-            </div>
-            <Textarea
-                class="min-h-24"
-                v-model="taskNote"
-                placeholder="添加备注"
-                @focus="editTemp = taskNote"
-                @blur="editGate(taskNote, () => {
-                updateTask(taskIndex, { note: taskNote })
-              })"
-            />
-          </div>
-          <div class="text-xs text-gray-500 italic">
-            <div>
-              <span v-if="!taskUpdateTime">创建于 {{ taskCreateTime }}</span>
-              <TooltipProvider v-else>
-                <Tooltip>
-                  <TooltipTrigger as-child>
-                  <span>最后更新于 {{ taskUpdateTime }}</span>
-                  </TooltipTrigger>
-                  <TooltipContent side="top">
-                    创建于 {{ taskCreateTime }}
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </div>
-            <div class="mt-2 flex items-center" v-if="taskFinishTime">
-              <Check class="size-4 text-green-500 mr-2" />
-              完成于 {{ taskFinishTime }}
-            </div>
-          </div>
+        <div class="mt-4" v-if="taskIndex">
+          <TaskDetail
+              v-model:taskIndex="taskIndex"
+              v-model:taskDescription="taskDescription"
+              v-model:taskNote="taskNote"
+              v-model:taskDueDate="taskDueDate"
+              v-model:taskCreateTime="taskCreateTime"
+              v-model:taskUpdateTime="taskUpdateTime"
+              v-model:taskFinishTime="taskFinishTime"
+              v-model:taskBelongList="taskBelongList"
+              v-model:lists="lists"
+              @update="updateTask"
+          />
         </div>
       </transition>
     </div>
-
+    <Drawer v-model:open="showTaskDetailDrawer" @update:open="onTaskDetailDrawerStateChange">
+      <DrawerContent>
+        <DrawerHeader class="hidden">
+          <DrawerTitle>Move Goal</DrawerTitle>
+          <DrawerDescription>aaa</DrawerDescription>
+        </DrawerHeader>
+        <div class="px-2 max-h-[60vh] overflow-auto">
+          <TaskDetail
+              v-if="taskIndex"
+              v-model:taskIndex="taskIndex"
+              v-model:taskDescription="taskDescription"
+              v-model:taskNote="taskNote"
+              v-model:taskDueDate="taskDueDate"
+              v-model:taskCreateTime="taskCreateTime"
+              v-model:taskUpdateTime="taskUpdateTime"
+              v-model:taskFinishTime="taskFinishTime"
+              v-model:taskBelongList="taskBelongList"
+              v-model:lists="lists"
+              @update="updateTask"
+          />
+        </div>
+      </DrawerContent>
+    </Drawer>
   </div>
 </template>
 
