eaca80edd89ac7db9804d3e02f08629f7e5d6358
Leon <ycliang.cn@outlook.com>
2024-06-17 23:10:35 +0100

    add prettier

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,32 +1,5 @@
-<script setup lang='ts'>
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="border-2 flex justify-center flex-wrap gap-2 p-4 h-full overflow-y-auto">
-
-    <Card class="h-72" v-for="i in 18">
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        Card Content
-      </CardContent>
-      <CardFooter>
-        Card Footer
-      </CardFooter>
-    </Card>
-
-
-
-  </div>
-
+  <div class="">dashboard</div>
 </template>
